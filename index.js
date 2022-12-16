62344476b6bafa402ce729f04a85ede758698d91
Tirsa St. Fort <lifelovelori@gmail.com>
2022-12-16 15:52:36 -0500

    commit tree 94486310fefd0e3303deafe70f8d078883aaa25c

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+    name: "John Smith",
+    streetAddress: "123 Main St",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployeeWithKeyAndValue = {...employee}
+    newEmployeeWithKeyAndValue[key] = value
+    return newEmployeeWithKeyAndValue
+}
+
+const newEmployeeWithKeyAndValue = updateEmployeeWithKeyAndValue(
+    employee,
+    "Sam",
+    "11 Broadway"
+    )
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+deleteFromEmployeeByKey(employee, "name")
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+    }
+
+destructivelyDeleteFromEmployeeByKey(employee, "name")
