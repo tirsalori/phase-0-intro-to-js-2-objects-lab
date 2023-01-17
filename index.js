6717d6f00638b741d223f4e15cc1785ad166a40a
Tirsa St. Fort <lifelovelori@gmail.com>
2023-01-16 20:21:07 -0800

    Completed lab.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "John Smith",
+    streetAddress: "123 Main Street" 
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]:value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]= value
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key]
+    return obj
+}
